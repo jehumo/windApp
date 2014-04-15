c1b35aa18a6e3d544748633c2a2e933938f0e4d8
Jesús Hurtado <jesushurtado@gmail.com>
2014-04-15 19:44:04 +0200

    Parsing a city

---
+++ iOSWindChallenge/Wind.m
@@ -0,0 +1,13 @@
+//
+//  Wind.m
+//  iOSWindChallenge
+//
+//  Created by JESUS HURTADO on 15/04/14.
+//  Copyright (c) 2014 Piksel. All rights reserved.
+//
+
+#import "Wind.h"
+
+@implementation Wind
+
+@end
---
+++ iOSWindChallenge/model/Weather.m
@@ -0,0 +1,13 @@
+//
+//  Weather.m
+//  iOSWindChallenge
+//
+//  Created by JESUS HURTADO on 15/04/14.
+//  Copyright (c) 2014 Piksel. All rights reserved.
+//
+
+#import "Weather.h"
+
+@implementation Weather
+
+@end
